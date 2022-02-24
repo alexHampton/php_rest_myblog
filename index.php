4e73d453355415f51cedfe70c10a9d62740ed577
Alex Hampton <ahampton5158@gmail.com>
2022-02-24 11:58:42 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+
+    </form>
+        <label for="searchbar"></label>
+        <input type="text" id="searchbar" name="searchbar">
+        <input type="submit">
+
+    </form>
+
+
+</body>
+
+</html>
